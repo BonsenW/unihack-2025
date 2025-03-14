8467c5d27267f3d6328188bee8a732146feb1d19
Linda Nguyen <lngu0107@student.monash.edu>
2025-03-15 04:24:18 +1100

    updated database uploader so that all keys are vectorized

--- backend/server.ts
+++ backend/server.ts
@@ -3,6 +3,8 @@ const express = require('express');
 const path = require('path');
 const app = express();
 import { connectDb } from './db'
+import { VectorizeDoc, Db, Collection } from "@datastax/astra-db-ts";
+import fs from "fs";
 
 const dummyData = require('./data/noIdDummyData.json')
 
@@ -21,6 +23,94 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
 });
 
+
+// Defining interface for user information to later parse
+// The VectorizeDoc interface adds a $vectorize key.
+export interface UserInfo extends VectorizeDoc {
+    "gender": string;
+    "age": string;
+    "interests": string;
+    "pets": string;
+    "budgetRange": string;
+    "garageSpace": string;
+};
+
+/**
+ * Creates a collection in the specified database with vectorization enabled.
+ * The collection will use NVIDIA's NV-Embed-QA embedding model
+ * to generate vector embeddings for data in the collection.
+ *
+ * @param database - The instantiated object that represents the database where the collection will be created.
+ * @param collectionName - The name of the collection to create.
+ * @returns A promise that resolves to the created collection.
+ */
+async function createCollection(
+  database: Db,
+  collectionName: string,
+): Promise<Collection<UserInfo>> {
+  const collection = await database.createCollection<UserInfo>(collectionName, {
+    vector: {
+      service: {
+        provider: "nvidia",
+        modelName: "NV-Embed-QA",
+      },
+    },
+  });
+
+  console.log(
+    `Created collection ${collection.keyspace}.${collection.collectionName}`,
+  );
+
+  return collection;
+}
+
+/**
+ * Uploads data from a file containing a JSON array to the specified collection.
+ * For each piece of data, a $vectorize field is added. The $vectorize value is
+ * a string from which vector embeddings will be generated.
+ *
+ * @param collection - The instantiated object that represents the collection to upload the data to.
+ * @param dataFilePath - The path to a JSON file containing a JSON array.
+ * @param embeddingStringCreator - A function to create the string for which vector embeddings will be generated.
+ * @returns {Promise<void>} A promise that resolves when the data has been uploaded.
+ */
+async function uploadJsonData(
+  collection: Collection<UserInfo>,
+  dataFilePath: string,
+  embeddingStringCreator: (data: Record<string, any>) => string,
+): Promise<void> {
+  // Read the JSON file and parse it into a JSON array.
+  const rawData = fs.readFileSync(dataFilePath, "utf8");
+  const jsonData = JSON.parse(rawData);
+
+  // Add a $vectorize field to each piece of data.
+  const documents: UserInfo[] = jsonData.map((data: any) => ({
+    ...data,
+    $vectorize: embeddingStringCreator(data),
+  }));
+
+  // Upload the data.
+  const inserted = await collection.insertMany(documents);
+  console.log(`Inserted ${inserted.insertedCount} items.`);
+}
+
+(async function () {
+  const database = connectDb();
+
+  const collection = await createCollection(
+    database,
+    "quickstart_collection",
+  );
+
+  await uploadJsonData(
+    collection,
+    "./data/noIdDummyData.json",
+    (data) => {
+      // Concatenate all the fields into one string to be vectorized
+      return `${data.gender} | ${data.age.join(", ")} | ${data.interests.join(", ")} | ${data.pets} | ${data.budgetRange} | ${data.garageSpace}`;
+    },
+  );
+})();
 
 
 const PORT = process.env.PORT || 5000;
