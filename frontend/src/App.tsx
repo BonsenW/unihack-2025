40a5462b3de4fad6ae0b5e876239098f971eaf8d
Bonsen Wakjira <59679148+BonsenW@users.noreply.github.com>
2025-03-15 21:27:44 +1100

    Refactoring landing page nav bar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,19 @@
-import HomePage from "./pages/HomePage";
-import LoginPage from "./pages/LoginPage";
-import LandingPage from "./pages/LandingPage";
+import { Routes, Route } from "react-router-dom";
+import LandingPageNavBar from "./components/custom/LandingPageNavBar";
 
 function App() {
     return (
-        <LandingPage></LandingPage>
+        <>
+            <LandingPageNavBar />
+            {/* <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/contact" element={<Contact />} />
+            </Routes> */}
+        </>
     );
 }
+
+
 
 export default App;
