bddeecdefcb69100f0073e5f1289c953ac07c07f
jazfantasia-monash <161562399+jazfantasia-monash@users.noreply.github.com>
2025-03-15 22:53:36 +1100

    created a few drop down menus that display age and gender preference

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,12 @@
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import LandingPage from "./pages/LandingPage";
+import ProfilePreferences from "./pages/ProfilePreferences";
 import { Home } from "lucide-react";
 
 function App() {
     return (
-        <HomePage></HomePage>
+        <ProfilePreferences></ProfilePreferences>
     );
 }
 
