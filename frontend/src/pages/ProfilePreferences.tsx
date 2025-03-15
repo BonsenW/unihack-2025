fe5d49d34f0b1f0d82274f4c132c258da8c1ade3
jazfantasia-monash <161562399+jazfantasia-monash@users.noreply.github.com>
2025-03-16 10:29:32 +1100

    pushed fixes and drop down menus for preferences

--- frontend/src/pages/ProfilePreferences.tsx
+++ frontend/src/pages/ProfilePreferences.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "@/components/ui/button" 
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import {
@@ -14,155 +14,128 @@ import "./ProfilePreferences.css";
 
 const ProfilePreferences: React.FC = () => {
     const [position, setPosition] = React.useState("bottom")
+    const [gender, setGender] = useState("");
+    const [age, setAge] = useState("");
+    const [location, setLocation] = useState("");
+    const [interests, setInterest] = useState("");
+    const [pets, setPets] = useState("");
+    const [budget, setBudget] = useState("");
+    const [garage, setGarage] = useState("");
+
 
     return (
         <>
-            <div className="flex flex-col gap-12 justify-center items-center w-screen h-screen bg-[#ECECDF]">
+            <Card className="card bg-[#ECECDF]">
+            <div className="flex flex-col gap-12 justify-center items-center w-screen min-h-screen bg-[#ECECDF]">
+                <div className="text-center pt-20">
+                    <CardTitle className="preferences-title"><b>Preferences</b></CardTitle>
+                    <p>Add your preferences so we can match you with most compatible room mate</p>
+                </div>
                 <Card className="w-150 p-6 rounded-2xl bg-[#ECECDF]/40">
-                    <CardHeader className="text-center">
-                        <CardTitle className="preferences-title text-3xl text-[#1F1F41]"><b>Preferences</b></CardTitle>
-                    </CardHeader>
                     
                     <CardContent className="flex flex-col gap-4 w-full bg-[#ECECDF]/20">
                         {/* Roommates Gender Preference */}
                         <h3 className="text-sm">Roommate Gender Preference</h3>
-                        <DropdownMenu>
-                            <DropdownMenuTrigger asChild className="age-dropdown bg-transparent rounded-md border border-3 border-black">
-                                <Button variant="outline"></Button>
-                            </DropdownMenuTrigger>
-
-                            <DropdownMenuContent className="w-125 bg-[#ECECDF] border border-black border-3">
-                                <DropdownMenuLabel>Gender</DropdownMenuLabel>
-                                <DropdownMenuSeparator />
-                                <DropdownMenuRadioGroup value={position} onValueChange={setPosition}>
-                                <DropdownMenuRadioItem value="male-preference">Male</DropdownMenuRadioItem>
-                                <DropdownMenuRadioItem value="female-preference">Female</DropdownMenuRadioItem>
-                                <DropdownMenuRadioItem value="no-preference">No preference</DropdownMenuRadioItem>
-                                </DropdownMenuRadioGroup>
-                                </DropdownMenuContent>
-                        </DropdownMenu>
+                        <select
+                            value={gender}
+                            onChange={(e) => setGender(e.target.value)}
+                            className="gender dropdown-field w-125 text-black bg-[#ECECDF] border-3 border-black rounded-md  pl-3 pr-10 py-2 appearance-none outline-none focus:ring-0 focus:border-black">
+                            <option value="">Select your gender</option>
+                            <option value="Male">Male</option>
+                            <option value="Female">Female</option>
+                            <option value="Prefer not to disclose">Prefer not to disclose</option>
+                        </select>
                         
+
                         {/* Roommates Age */}
                         <h3 className="text-sm">Roommate Age</h3>
-                        <DropdownMenu>
-                            <DropdownMenuTrigger asChild className="age-dropdown bg-transparent rounded-md border border-3 border-black">
-                                <Button variant="outline"></Button>
-                            </DropdownMenuTrigger>
+                        <select
+                            value={age}
+                            onChange={(e) => setAge(e.target.value)}
+                            className="age dropdown-field w-125 text-black bg-[#ECECDF] border-3 border-black rounded-md  pl-3 pr-10 py-2 appearance-none outline-none focus:ring-0 focus:border-black">
+                            <option value="">Select age range</option>
+                            <option value="18-20">18-20</option>
+                            <option value="21-24">21-24</option>
+                            <option value="25-30">25-30</option>
+                            <option value="31-34">31-34</option>
+                            <option value="35-plus">35+</option>
+                        </select>
 
-                            <DropdownMenuContent className="w-125 bg-[#ECECDF] border border-black border-3">
-                                <DropdownMenuLabel>Age Range</DropdownMenuLabel>
-                                <DropdownMenuSeparator />
-                                <DropdownMenuRadioGroup value={position} onValueChange={setPosition}>
-                                <DropdownMenuRadioItem value="18-20">18-20</DropdownMenuRadioItem>
-                                <DropdownMenuRadioItem value="21-24">21-24</DropdownMenuRadioItem>
-                                <DropdownMenuRadioItem value="25-30">25-30</DropdownMenuRadioItem>
-                                <DropdownMenuRadioItem value="31-34">31-34</DropdownMenuRadioItem>
-                                <DropdownMenuRadioItem value="35-plus">35+</DropdownMenuRadioItem>
-                                </DropdownMenuRadioGroup>
-                                </DropdownMenuContent>
-                        </DropdownMenu>
-
-                        {/* House Location */}
-                        <h3 className="text-sm">House Location</h3>
-                        <DropdownMenu>
-                            <DropdownMenuTrigger asChild className="age-dropdown bg-transparent rounded-md border border-3 border-black">
-                                <Button variant="outline"></Button>
-                            </DropdownMenuTrigger>
-
-                            <DropdownMenuContent className="w-125 bg-[#ECECDF] border border-black border-3">
-                                <DropdownMenuLabel>Suburbs</DropdownMenuLabel>
-                                <DropdownMenuSeparator />
-                                <DropdownMenuRadioGroup value={position} onValueChange={setPosition}>
-                                <DropdownMenuRadioItem value="suburb-clayton">Clayton</DropdownMenuRadioItem>
-                                <DropdownMenuRadioItem value="suburb-huntingdale">Huntingdale</DropdownMenuRadioItem>
-                                <DropdownMenuRadioItem value="suburb-oakleigh">Oakleigh</DropdownMenuRadioItem>
-                                <DropdownMenuRadioItem value="suburb-murumbeena">Murumbeena</DropdownMenuRadioItem>
-                                <DropdownMenuRadioItem value="suburb-hughesdale">Hughesdale</DropdownMenuRadioItem>
-                                </DropdownMenuRadioGroup>
-                                </DropdownMenuContent>
-                        </DropdownMenu>
+                        {/* Roommate Location */}
+                        <h3 className="text-sm">House Location </h3>
+                        <select
+                            value={location}
+                            onChange={(e) => setLocation(e.target.value)}
+                            className="location dropdown-field w-125 text-black bg-[#ECECDF] border-3 border-black rounded-md  pl-3 pr-10 py-2 appearance-none outline-none focus:ring-0 focus:border-black">
+                            <option value="">Select your Location</option>
+                            <option value="suburb-clayton">Clayton</option>
+                            <option value="suburb-huntingdale">Huntingdale</option>
+                            <option value="suburb-oakleigh">Oakleigh</option>
+                            <option value="suburb-murumbeena">Murumbeena</option>
+                            <option value="suburb-hughesdale">Hughesdale</option>
+                        </select>
 
                         {/* Interests */}
-                        <h3 className="text-sm">Interests</h3>
-                        <DropdownMenu>
-                            <DropdownMenuTrigger asChild className="age-dropdown bg-transparent rounded-md border border-3 border-black">
-                                <Button variant="outline"></Button>
-                            </DropdownMenuTrigger>
+                        <h3 className="text-sm">Interests </h3>
+                        <select
+                            value={interests}
+                            onChange={(e) => setInterest(e.target.value)}
+                            className="dropdown-field w-125 text-black bg-[#ECECDF] border-3 border-black rounded-md  pl-3 pr-10 py-2 appearance-none outline-none focus:ring-0 focus:border-black">
+                            <option value="">Select your interests</option>
+                            <option value="interests-games">Games</option>
+                            <option value="interests-cooking">Cooking</option>
+                            <option value="interests-food">Food</option>
+                            <option value="interests-photography">Photography</option>
+                            <option value="interests-music">Music</option>
+                            <option value="interests-dance">Dance</option>
+                        </select>
 
-                            <DropdownMenuContent className="w-125 bg-[#ECECDF] border border-black border-3">
-                                <DropdownMenuLabel>Types of Interests</DropdownMenuLabel>
-                                <DropdownMenuSeparator />
-                                <DropdownMenuRadioGroup value={position} onValueChange={setPosition}>
-                                <DropdownMenuRadioItem value="interests-games">Games</DropdownMenuRadioItem>
-                                <DropdownMenuRadioItem value="interests-cooking">Cooking</DropdownMenuRadioItem>
-                                <DropdownMenuRadioItem value="interests-food">Food</DropdownMenuRadioItem>
-                                <DropdownMenuRadioItem value="interests-photography">Photography</DropdownMenuRadioItem>
-                                <DropdownMenuRadioItem value="interests-sports">Sports</DropdownMenuRadioItem>
-                                <DropdownMenuRadioItem value="interests-music">Music</DropdownMenuRadioItem>
-                                <DropdownMenuRadioItem value="interests-music">Dance</DropdownMenuRadioItem>
-                                </DropdownMenuRadioGroup>
-                                </DropdownMenuContent>
-                        </DropdownMenu>
-
-                        {/* Pets */}
-                        <h3 className="text-sm">Pets</h3>
-                        <DropdownMenu>
-                            <DropdownMenuTrigger asChild className="age-dropdown bg-transparent rounded-md border border-3 border-black">
-                                <Button variant="outline"></Button>
-                            </DropdownMenuTrigger>
-
-                            <DropdownMenuContent className="w-125 bg-[#ECECDF] border border-black border-3">
-                                <DropdownMenuLabel>Types of pets</DropdownMenuLabel>
-                                <DropdownMenuSeparator />
-                                <DropdownMenuRadioGroup value={position} onValueChange={setPosition}>
-                                <DropdownMenuRadioItem value="pets-cat">Cat</DropdownMenuRadioItem>
-                                <DropdownMenuRadioItem value="pets-dog">Dog</DropdownMenuRadioItem>
-                                <DropdownMenuRadioItem value="pets-other">Other</DropdownMenuRadioItem>
-                                <DropdownMenuRadioItem value="pets-no-pref">No preference</DropdownMenuRadioItem>
-                                <DropdownMenuRadioItem value="pets-no-pets">No pets</DropdownMenuRadioItem>
-                                </DropdownMenuRadioGroup>
-                                </DropdownMenuContent>
-                        </DropdownMenu>
+                        {/* Interests */}
+                        <h3 className="text-sm">Pets </h3>
+                        <select
+                            value={pets}
+                            onChange={(e) => setPets(e.target.value)}
+                            className="dropdown-field w-125 text-black bg-[#ECECDF] border-3 border-black rounded-md  pl-3 pr-10 py-2 appearance-none outline-none focus:ring-0 focus:border-black">
+                            <option value="">Select your pets</option>
+                            <option value="pets-cat">Cat</option>
+                            <option value="pets-dog">Dog</option>
+                            <option value="pets-other">Other</option>
+                            <option value="pets-no-preference">No preferences</option>
+                            <option value="pets-no-pets">No pets</option>
+                        </select>
+                        
 
                         {/* Budget Range */}
                         <h3 className="text-sm">Budget Range</h3>
-                        <DropdownMenu>
-                            <DropdownMenuTrigger asChild className="age-dropdown bg-transparent rounded-md border border-3 border-black">
-                                <Button variant="outline"></Button>
-                            </DropdownMenuTrigger>
-
-                            <DropdownMenuContent className="w-125 bg-[#ECECDF] border border-black border-3">
-                                <DropdownMenuLabel>Range of prices</DropdownMenuLabel>
-                                <DropdownMenuSeparator />
-                                <DropdownMenuRadioGroup value={position} onValueChange={setPosition}>
-                                <DropdownMenuRadioItem value="budget-200">Less than 200</DropdownMenuRadioItem>
-                                <DropdownMenuRadioItem value="budget-200-300">200-300</DropdownMenuRadioItem>
-                                <DropdownMenuRadioItem value="budget-300-400">300-400</DropdownMenuRadioItem>
-                                <DropdownMenuRadioItem value="budget-400">More than 400</DropdownMenuRadioItem>
-                                </DropdownMenuRadioGroup>
-                                </DropdownMenuContent>
-                        </DropdownMenu>
+                        <select
+                            value={budget}
+                            onChange={(e) => setBudget(e.target.value)}
+                            className="dropdown-field w-125 text-black bg-[#ECECDF] border-3 border-black rounded-md  pl-3 pr-10 py-2 appearance-none outline-none focus:ring-0 focus:border-black">
+                            <option value="">Select your budget</option>
+                            <option value="budget-200">Less than 200</option>
+                            <option value="budget-200-300">200-300</option>
+                            <option value="budget-300-400">300-400</option>
+                            <option value="budget-400">More than 400</option>
+                        </select>
 
                         {/* Garage Space */}
                         <h3 className="text-sm">Garage Space</h3>
-                        <DropdownMenu>
-                            <DropdownMenuTrigger asChild className="age-dropdown bg-transparent rounded-md border border-3 border-black">
-                                <Button variant="outline"></Button>
-                            </DropdownMenuTrigger>
-
-                            <DropdownMenuContent className="w-125 bg-[#ECECDF] border border-black border-3">
-                                <DropdownMenuLabel>Garage Preferences</DropdownMenuLabel>
-                                <DropdownMenuSeparator />
-                                <DropdownMenuRadioGroup value={position} onValueChange={setPosition}>
-                                <DropdownMenuRadioItem value="garage-one-car">1 car</DropdownMenuRadioItem>
-                                <DropdownMenuRadioItem value="garage-two-car">2 cars</DropdownMenuRadioItem>
-                                <DropdownMenuRadioItem value="garage-na">N/A</DropdownMenuRadioItem>
-                                </DropdownMenuRadioGroup>
-                                </DropdownMenuContent>
-                        </DropdownMenu>
+                        <select
+                            value={budget}
+                            onChange={(e) => setBudget(e.target.value)}
+                            className="dropdown-field w-125 text-black bg-[#ECECDF] border-3 border-black rounded-md  pl-3 pr-10 py-2 appearance-none outline-none focus:ring-0 focus:border-black">
+                            <option value="">Select your budget</option>
+                            <option value="garage-one-car">1 car</option>
+                            <option value="garage-two-car">2 cars</option>
+                            <option value="garage-na">N/A</option>
+                        </select>
                     </CardContent>  
                 </Card>
-            </div>
+                <div className="flex justify-center mt-6"> {/* Added wrapper div to center button */}
+                                <Button className="save-button">Save Changes</Button>
+                </div>
+            </div>  
+            </Card>
         </>
     );
 };
