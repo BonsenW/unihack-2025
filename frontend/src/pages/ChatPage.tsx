d8df4fea920f5da4e524f1be8994d26bf0b2ba31
Elsie-Shum <130987602+Elsie-Shum@users.noreply.github.com>
2025-03-16 00:30:26 +1100

    Finished styling chatPage

--- frontend/src/pages/ChatPage.tsx
+++ frontend/src/pages/ChatPage.tsx
@@ -7,14 +7,17 @@ const ChatPage: React.FC = () => {
         <div className="chatPage">
             <div className='chat-left'>
                 <div className='profile-detail'>
-                    <h1 id="user-name">Name</h1>
-                    <p>Direct Messages</p>
+                    <h1>Name</h1>
+                    <p id="direct-msg">Direct Messages</p>
                 </div>
             </div>
             <div className='chat-right'>
-                <h1>Friend name</h1>
-                <p>Description</p>
+                <div className='friend-detail'>
+                    <h1>Friend name</h1>
+                    <p>Description</p>
+                </div>
                 <div className="msg-container">
+                    <div className="messages"></div>
                     <form className="send-container">
                         <input type="text" id="message-input"></input>
                         <Button type="submit" id="send-button">Send</Button>
