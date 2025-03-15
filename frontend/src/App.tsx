d212fbe6f9a106614f169675dbeed2e79130be1a
yfes0001 <yfesshaye01@gmail.com>
2025-03-15 21:46:45 +1100

    started html and css for profile page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,11 @@
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import LandingPage from "./pages/LandingPage";
+import ProfilePage from "./pages/ProfilePage";
 
 function App() {
     return (
-        <LandingPage></LandingPage>
+        <ProfilePage></ProfilePage>
     );
 }
 
---
+++ frontend/src/pages/ProfilePage.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./ProfilePage.css";  // Your custom styles
+import { Button } from "@/components/ui/button";
+
+const ProfilePage = () => {
+    return (
+        <>
+        <div className="flex justify-between">
+            <div>
+                <h1>Edit Profile</h1>
+            </div>
+        </div>
+        </>
+    );
+};
+
+export default ProfilePage;
