00a133508fb87b5aa603a9d54cc9a0b79debddb2
Bonsen Wakjira <59679148+BonsenW@users.noreply.github.com>
2025-03-15 11:35:21 +1100

    Finished login and cleaned it up

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,23 +1,26 @@
-import './Login.css';
+import "./Login.css";
 
 function Login() {
   return (
-    <div className='login'>
-        <div className='login-card'>
-            <button>Back</button>
-            <h4>Housemate</h4>
-            <h2>Login</h2>
-            <p>access to your account</p>
-            <input>Username</input>
-            <input>Password</input>
-            <button>Login</button>
-        </div>
-        <div className='idk'>
-            <p>Find your connection</p>
-            <p>New here? <a>Join Now</a></p>
-        </div>
+    <div className="login">
+      <div className="login-card">
+        <button className="back-btn">Back</button>
+        <h4>Housemate</h4>
+        <h2>Login</h2>
+        <p>Access your account</p>
+        
+        <input type="text" placeholder="Username" />
+        <input type="password" placeholder="Password" />
+        
+        <button className="login-btn">Login</button>
+      </div>
+
+      <div className="login-footer">
+        <p>Find your connection</p>
+        <p>New here? <a href="#">Join Now</a></p>
+      </div>
     </div>
   );
 }
 
-export default App;
+export default Login;
