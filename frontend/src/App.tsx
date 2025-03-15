17bd10355b1c98be2dc7e2ac62480b43e9e3db87
Bonsen Wakjira <59679148+BonsenW@users.noreply.github.com>
2025-03-15 17:47:22 +1100

    Created and finalised Login page, cleaned up repo and old code

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,8 @@
-import "./App.css";
-import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage"
 
 function App() {
     return (
-        <HomePage></HomePage>
+        <LoginPage></LoginPage>
     );
 }
 
