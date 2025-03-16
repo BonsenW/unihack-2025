4579625ced18b0da22ae8e1487522ba1a76c8f13
Ricky Z <rickyzhang2013@hotmail.com>
2025-03-16 11:53:48 +1100

    changes

--- backend/server.ts
+++ backend/server.ts
@@ -4,8 +4,7 @@ dotenv.config()
 import path from 'path';
 const app = express();
 import { connectDb } from "./db.js";
-import { VectorizeDoc, Db, Collection } from "@datastax/astra-db-ts";
-import fs from "fs";
+
 
 import { initializeLangflow } from './langflow.js';
 import { InitializeMongoDb } from './mongo.js';
@@ -29,87 +28,7 @@ app.use(express.static(path.join(__dirname, '../frontend/build')));
 
 
 
-
-/**
- * Creates a collection in the specified database with vectorization enabled.
- * The collection will use NVIDIA's NV-Embed-QA embedding model
- * to generate vector embeddings for data in the collection.
- *
- * @param database - The instantiated object that represents the database where the collection will be created.
- * @param collectionName - The name of the collection to create.
- * @returns A promise that resolves to the created collection.
- */
-// async function createCollection(
-//   database: Db,
-//   collectionName: string,
-// ): Promise<Collection<UserInfo>> {
-//   const collection = await database.createCollection<UserInfo>(collectionName, {
-//     vector: {
-//       service: {
-//         provider: "nvidia",
-//         modelName: "NV-Embed-QA",
-//       },
-//     },
-//   });
-
-//   console.log(
-//     `Created collection ${collection.keyspace}.${collection.collectionName}`,
-//   );
-
-//   return collection;
-// }
-
-// /**
-//  * Uploads data from a file containing a JSON array to the specified collection.
-//  * For each piece of data, a $vectorize field is added. The $vectorize value is
-//  * a string from which vector embeddings will be generated.
-//  *
-//  * @param collection - The instantiated object that represents the collection to upload the data to.
-//  * @param dataFilePath - The path to a JSON file containing a JSON array.
-//  * @param embeddingStringCreator - A function to create the string for which vector embeddings will be generated.
-//  * @returns {Promise<void>} A promise that resolves when the data has been uploaded.
-//  */
-// async function uploadJsonData(
-//   collection: Collection<UserInfo>,
-//   dataFilePath: string,
-//   embeddingStringCreator: (data: Record<string, any>) => string,
-// ): Promise<void> {
-//   // Read the JSON file and parse it into a JSON array.
-//   const rawData = fs.readFileSync(dataFilePath, "utf8");
-//   const jsonData = JSON.parse(rawData);
-
-//   // Add a $vectorize field to each piece of data.
-//   const documents: UserInfo[] = jsonData.map((data: any) => ({
-//     ...data,
-//     $vectorize: embeddingStringCreator(data),
-//   }));
-
-//   // Upload the data.
-//   const inserted = await collection.insertMany(documents);
-//   console.log(`Inserted ${inserted.insertedCount} items.`);
-// }
-
-// (async function () {
-//   const database = connectDb();
-
-//   const collection = await createCollection(
-//     database,
-//     "user_preferences",
-//   );
-
-//   await uploadJsonData(
-//     collection,
-//     './data/dummyData.json',
-//     (data) => {
-//       // Concatenate all the fields into one string to be vectorized
-//       return `${data.userId} | ${data.gender} | ${data.age.join(", ")} | ${data.interests.join(", ")} | ${data.pets} | ${data.budgetRange} | ${data.garageSpace}`;
-//     },
-//   );
-// })();
-
-
 const PORT = process.env.PORT || 5000;
-
 
 
 // Start server
@@ -119,46 +38,11 @@ const startServer = async () => {
     await initializeLangflow();
     await InitializeMongoDb();
 
-
-
     // const userCollection = db.collection('user_preferences');
-
-
     // userCollection.insertMany(dummyData)
-
 
     app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
 };
-
-// let inputValue = ""; // Insert input value here
-
-// fetch(
-//   "https://api.langflow.astra.datastax.com/lf/264dd2c4-6626-4af0-9f43-ef569e57dc2c/api/v1/run/f83f4261-bab2-47c7-9330-22f0b2b6444e?stream=false",
-//   {
-//     method: "POST",
-//     headers: {
-//       "Authorization": `Bearer ${process.env.DATASTAX_TOKEN}`,
-//       "Content-Type": "application/json"
-//     },
-//     body: JSON.stringify({
-// 			input_value: inputValue, 
-//       output_type: "chat",
-//       input_type: "chat",
-//       tweaks: {
-//         "ChatInput-DTz08": {},
-//         "Prompt-zjQZw": {},
-//         "ChatOutput-QRH2J": {},
-//         "ParseData-wovQq": {},
-//         "GoogleGenerativeAIModel-AfoqL": {},
-//         "AstraDB-lTea5": {}
-// }
-//     }),
-//   },
-// )
-//   .then(res => res.json())
-//   .then(data => console.log(data))
-//   .catch(error => console.error('Error:', error));
-
 
 startServer();
 
