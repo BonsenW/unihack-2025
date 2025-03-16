2a5dc49495b94b07b612ce5bc3923f31019fbd80
jazfantasia-monash <161562399+jazfantasia-monash@users.noreply.github.com>
2025-03-16 11:21:06 +1100

    reverted the directory back after testing purposes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,6 @@ import { Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import LoginPage from "./pages/LoginPage";
 import HomePage from "./pages/HomePage";
-import ProfilePreferences from "./pages/ProfilePreferences";
 
 const About = () => <h2>About Page</h2>;
 const Contact = () => <h2>Contact Page</h2>;
@@ -10,7 +9,13 @@ const Contact = () => <h2>Contact Page</h2>;
 function App() {
     return (
         <>
-            <ProfilePreferences></ProfilePreferences>
+            <Routes>
+                <Route path="/" element={<LandingPage />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/contact" element={<Contact />} />
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/home" element={<HomePage />} />
+            </Routes>
         </>
     );
 }
